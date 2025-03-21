575182f6206db9f6a0a0b95a7cf590a3f3c91980
Yeon / Ray👻 <wsyeon.may@gmail.com>
2025-03-21 12:25:14 +0900

    Create new project
    Create new project about Advanced test

---
+++ 250321_Advanced Test/250321_Advanced Test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _250321_Advanced_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
